3dd1dea9efcbab693edd1d5c4cf4e75d7d727c93
gustavberggren96 <s305067@stud.hioa.no>
2017-04-20 16:02:07 +0200

    Set correct timezone

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set("France/Paris");
+date_default_timezone_set("Europe/Oslo");
 echo "Server IP: " . $_SERVER['SERVER_ADDR'] . "</br>";
 echo "This line checks wether or not the web-servers is synced at ". date("h:i:sa") . "</br>";
 ?>
