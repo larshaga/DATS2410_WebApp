4cae74ec90b8e1913dd5597a5a23212c7f9f6bfc
gustavberggren96 <s305067@stud.hioa.no>
2017-04-20 16:06:26 +0200

    Adding am

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 date_default_timezone_set("Europe/Oslo");
 echo "Server IP: " . $_SERVER['SERVER_ADDR'] . "</br>";
-echo "This line checks wether or not the web-servers is synced at ". date("h:i") . "</br>";
+echo "This line checks wether or not the web-servers is synced at ". date("h:ia") . "</br>";
 ?>
