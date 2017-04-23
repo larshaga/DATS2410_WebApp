582af4c6669763d5e38448b84e9fc623f6acaefc
LarsKristianHaga <LarsKHaga@gmail.com>
2017-04-23 10:55:40 +0200

    a template we can work from

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
+<!doctype HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Dats04 - Web</title>
+</head>
+
+<body>
+  <div class="title">
+    <h1>Tittel</h1>
+  </div>
+
+  <div class="navigation">
+    <a href="index.html">All</a>
+    <a href="index.html">other</a>
+    <a href="index.html">data</a>
+    <!-- Add aditional fields here-->
+
+  </div>
+
+  <div class="login">
+    <form method="post">
+      <input type="Text" placeholder="Name">
+      <input type="Text" placeholder="Other data">
+      <input type="Text" placeholder="Other data">
+      <input type="Text" placeholder="Other data">
+      <input type="Submit">
+      <!-- Add aditional fields here-->
+    </form>
+  </div>
+</body>
+
+<footer>
+<p class="bottom">A webpage by students at Oslo and Akershus university college of applied sciences</p>
 <?php
-date_default_timezone_set("Europe/Oslo");
-echo "Server IP: " . $_SERVER['SERVER_ADDR'] . "</br>";
-echo "This line checks wether or not the web-servers is synced at ". date("h:ia") . "</br>";
+echo "The web server IP:" . $_SERVER['SERVER_ADDR'] . "<br>";
+echo "The database server IP: "<br>";
 ?>
+</footer>
+</html>
