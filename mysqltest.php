8dae87be57c0e613b3529fece44c3d29a88de69e
Jesper Nylend <jesper.nylend@gmail.com>
2017-04-26 07:52:30 +0200

    added testmysql to check how to do a query

---
+++ mysqltest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jesper Nylend
+ * Date: 26.04.2017
+ * Time: 07.42
+ */
+
+$host="10.1.1.130";
+$db="testdb";
+$user="testuser"; $pw="dats04";
+$dbconn = new mysqli($host, $user, $pw, $db);
+
+$sql = "SELECT * from test1";
+$result = $dbconn->query($sql);
+
+echo "Student navn:<br />";
+echo "<table border='1'>";
+echo "<tr><td>Student fornavn</td><td>Student etternavn</td></tr>";
+
+while ($row = $result->fetch_assoc()) {
+    echo "<tr><td>{$row['fornavn']}</td> <td>{$row['etternavn']}</td></tr>";
+}
+echo "</table>";
+
+$result->close();
+$dbconn->close();
+?>
