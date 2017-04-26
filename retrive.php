f261340a3fdc511f4d7e872a77045f8dcfc69587
trygvevang <trygve.n.vang@gmail.com>
2017-04-26 14:19:58 +0200

    Starting the retrieve page

--- retrive.php
+++ retrive.php
@@ -22,6 +22,76 @@
     <p>Retrive</p>
   </div>
 
+  <div class="form_div">
+      <form id="selectForm" method="get">
+          <select name="selectInfo">
+              <option value="1">Show info about student</option>
+              <option value="2">Show all students in a course</option>
+              <option value="3">Show every student who has taken a course</option>
+              <option value="4">Show all students in a study program</option>
+          </select>
+          <input class="dblock" type="Submit" value="Submit">
+      </form>
+      <?php
+      function StudentInfo()
+      {
+          echo "<div class=\"form_div\">
+                    <form method=\"post\">
+                        <input class=\"dblock\" type=\"Text\" placeholder=\"What is the student number?\">
+                        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
+                    </form>
+                </div>
+               ";
+      }
+
+      function StudentInCourse()
+      {
+          echo "
+                <div class=\"form_div\">
+                    <form method=\"post\">
+                        <input class=\"dblock\" type=\"Text\" placeholder=\"What is the course name?\">
+                        <input class=\"dblock\" type=\"Text\" placeholder=\"What year?\">
+                        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
+                    </form>
+                </div>
+               ";
+      }
+
+      function AllStudentsInCourse()
+      {
+          echo "
+                <div class=\"form_div\">
+                    <form method=\"post\">
+                        <input class=\"dblock\" type=\"Text\" placeholder=\"What is the course name?\">
+                        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
+                    </form>
+                </div>
+                ";
+      }
+
+      function AllStudentsInProgram()
+      {
+          echo "
+                <div class=\"form_div\">
+                    <form method=\"post\">
+                        <input class=\"dblock\" type=\"Text\" placeholder=\"What is the study program?\">
+                        <input class=\"dblock\" type=\"Text\" placeholder=\"What year?\">
+                        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
+                    </form>
+                </div>
+               ";
+      }
+
+      if (isset($_GET["selectInfo"]))
+      {
+          $info = $_GET["selectInfo"];
+          if ($info=="1") StudentInfo();
+          elseif ($info=="2") StudentInCourse();
+          elseif ($info=="3") AllStudentsInCourse();
+          else AllStudentsInProgram();
+      }
+      ?>
+  </div>
   <!--
   <div class="form_div">
     <form method="post">
