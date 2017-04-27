c813e5715147123a94a22dbd2205b4b06aded656
Jesper Nylend <jesper.nylend@gmail.com>
2017-04-27 14:17:22 +0200

    studentinfo.php now works

--- studentinfo.php
+++ studentinfo.php
@@ -29,50 +29,49 @@
 <div>
     <?php
 
-//    $stID=$_GET['stID'];
+    $stID=$_GET['stID'];
     //Connection to dats04-dbproxy
-//    $host="10.1.1.130";
-//    $user="webuser";
-//    $pw="welcomeunclebuild";
-//    $db="studentinfosys";
-//    $dbconn = new mysqli($host, $user, $pw, $db);
+    $host="10.1.1.130";
+    $user="webuser";
+    $pw="welcomeunclebuild";
+    $db="studentinfosys";
+    $dbconn = new mysqli($host, $user, $pw, $db);
 
-//    $sql = "select s.stID, concat(s.lastname,', ',s.firstname) as name, s.email from Student s where s.stID=$stID";
-//    $result = $dbconn->query($sql);
+    $sql = "select s.stID, concat(s.lastname,', ',s.firstname) as name, s.email from Student s where s.stID=$stID";
+    $result = $dbconn->query($sql);
 
     echo "<table border='1'>";
     echo "<tr><td>StudentID</td><td>Name</td><td>E-mail</td></tr>";
 
-//    while ($row = $result->fetch_assoc()) {
-//        echo "<tr><td>{$row['stID']}</td><td>{$row['name']}</td> <td>{$row['email']}</td></tr>";
-//    }
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row['stID']}</td><td>{$row['name']}</td> <td>{$row['email']}</td></tr>";
+    }
     echo "</table>";
 
-    //    $sql = "select p.title, e.startyear as 'Start Year' from Student s, Enrollment e, Study_program p where s.stID=e.stID and e.progcode=p.progcode and s.stID=$stID";
-    //    $result = $dbconn->query($sql);
+        $sql = "select p.title, e.startyear as 'Start Year' from Student s, Enrollment e, Study_program p where s.stID=e.stID and e.progcode=p.progcode and s.stID=$stID";
+        $result = $dbconn->query($sql);
 
     echo "<table border='1'>";
     echo "<tr><td>Study Program</td><td>Enrolled (Year)</td><tr>";
 
-//    while ($row = $result->fetch_assoc()) {
-//        echo "<tr><td>{$row['Start Year']}</td><td>{$row['year']}</td></tr>";
-//    }
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row['Start Year']}</td><td>{$row['year']}</td></tr>";
+    }
     echo "</table>";
 
-//    $sql = "select c.title as 'Course name', g.year, g.grade from Student s, Grade g, Course c where s.stID=g.stID and
-//    g.coursecode=c.coursecode and g.year=c.year and s.stID=XXXXXXXXX;";
-//    $result = $dbconn->query($sql);
+    $sql = "select c.title as 'Course name', g.year, g.grade from Student s, Grade g, Course c where s.stID=g.stID and g.coursecode=c.coursecode and g.year=c.year and s.stID=$stID";
+    $result = $dbconn->query($sql);
 
     echo "<table border='1'>";
     echo "<tr><td>Course Name</td><td>Year</td><td>Grade</td></tr>";
 
-//    while ($row = $result->fetch_assoc()) {
-//        echo "<tr><td>{$row['Course name']}</td><td>{$row['year']}</td> <td>{$row['grade']}</td></tr>";
-//    }
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row['Course name']}</td><td>{$row['year']}</td> <td>{$row['grade']}</td></tr>";
+    }
     echo "</table>";
 
-//    $result->close();
-//    $dbconn->close();
+    $result->close();
+    $dbconn->close();
     ?>
 </div>
 </body>
