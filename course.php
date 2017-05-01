c094c8274b547959969b61db73903ccd72a11f28
trygvevang <trygve.n.vang@gmail.com>
2017-05-01 10:19:24 +0200

    Adding back to student button

--- course.php
+++ course.php
@@ -6,198 +6,198 @@
 </head>
 
 <body>
-<div class="title">
-    <h1>Every course in the database</h1>
-</div>
+    <div class="title">
+        <h1>Every course in the database</h1>
+    </div>
 
-<div>
-    <a class="navigation" href="index.php">Home</a>
-    <a class="navigation" href="student.php">Student</a>
-    <a class="currentpage">Course</a>
-    <a class="navigation" href="studyprogram.php">Study program</a>
-</div>
+    <div>
+        <a class="navigation" href="index.php">Home</a>
+        <a class="navigation" href="student.php">Student</a>
+        <a class="currentpage">Course</a>
+        <a class="navigation" href="studyprogram.php">Study program</a>
+    </div>
 
-<div class="siteinfo">
-    <form id="selectForm" method="get">
-        <select name="selectInfo">
-            <option value="1">Search for course by coursecode</option>
-            <option value="2">Show every course in database</option>
-            <option value="3">Add new course</option>
-        </select>
-        <input class="dblock" type="Submit" value="Submit">
-    </form>
+    <div class="siteinfo">
+        <form id="selectForm" method="get">
+            <select name="selectInfo">
+                <option value="1">Search for course by coursecode</option>
+                <option value="2">Show every course in database</option>
+                <option value="3">Add new course</option>
+            </select>
+            <input class="dblock" type="Submit" value="Submit">
+        </form>
 
-    <?php
-    ob_start();
+        <?php
+            ob_start();
 
-    //Connection to dats04-dbproxy
-    $host="10.1.1.130";
-    $user="webuser";
-    $pw="welcomeunclebuild";
-    $db="studentinfosys";
-    $dbconn = new mysqli($host, $user, $pw, $db);
+            //Connection to dats04-dbproxy
+            $host="10.1.1.130";
+            $user="webuser";
+            $pw="welcomeunclebuild";
+            $db="studentinfosys";
+            $dbconn = new mysqli($host, $user, $pw, $db);
 
-    EveryCourse($dbconn); //As a standard show every course, but when something else is chosen, it disappears.
+            EveryCourse($dbconn); //As a standard show every course, but when something else is chosen, it disappears.
 
-    function EveryCourse($dbconn)
-    {
-        ob_clean();
-
-        $sql = "SELECT * from Course ORDER BY year DESC";
-        $result = $dbconn->query($sql);
-        echo "<table class='form_div'>";
-        echo "<tr><td>Course code</td><td>Year</td><td>Title</td><td>Action</td></tr>";
-        while ($row = $result->fetch_assoc())
-        {
-            echo "<tr><td>{$row['coursecode']}</td><td>{$row['year']}</td><td>{$row['title']}</td>
-                <td>
-                    <form action=\"courseinfo.php\" method=\"GET\">
-                        <input type='hidden' name='coursecode' value={$row['coursecode']}>
-                        <input type='hidden' name='coursetitle' value={$row['title']}>
-                        <input type='hidden' name='year' value={$row['year']}>
-                        <input type=\"submit\" value=\"Show courseinfo\">
-                    </form></td>
-                    <td><form action=\"courseedit.php\" method=\"GET\">
-                        <input type='hidden' name='editCourse' value={$row['coursecode']}>
-                        <input type='submit' value=\"Edit\">                
-                    </form>
-                </td><td><form method=\"GET\">
-                        <input type='hidden' name='deletecourse' value='1'>
-                        <input type='hidden' name='coursecode' value={$row['coursecode']}>
-                        <input type='hidden' name='year' value={$row['year']}>
-                        <input type='submit' value=\"Delete\">                
-                    </form>
-                </td></tr>";
-        }
-        echo "</table>";
-    }
-
-    function AddCourse()
-    {
-        ob_clean();
-
-        echo "
-                <form method='GET'>
-                    <p>Course code (four letteres):</p>
-                    <input name='coursecode' type='text' maxlength='4' required>
-                    
-                    <p>What year is this course run:</p>
-                    <input name='courseyear' type='number' min='1000' max='".date("Y")."' required>
-                    
-                    <p>Title of course:</p>
-                    <input name='coursetitle' type='text' required>
-                    
-                    <input type='submit' value='Add course'>
-                </form>";
-    }
-
-    function AddNewCourse($coursecode, $courseyear, $coursetitle, $dbconn)
-    {
-
-        /* Regular expressions to check if input is valid. */
-        $codepattern = "[a-zA-Z]{4}";
-        $titlepattern = "[a-zA-Z]+";
-        $yearpattern = "[1-2][0-9]{3}";
-        $coursecode = strtoupper($coursecode); //Makes sure course-code is uppercase.
-        ob_clean();
-        if (sizeof(preg_match($codepattern, $coursecode)) == 1 && sizeof(preg_match($titlepattern, $coursetitle)) == 1 && sizeof(preg_match($yearpattern, $courseyear)) == 1)
-        {
-            $sql = "INSERT INTO Course VALUES ('$coursecode', '$courseyear', '$coursetitle')";
-            if ($result = $dbconn->query($sql) === TRUE)
+            function EveryCourse($dbconn)
             {
-                echo "<p>Course was successcully added.</p>";
-            } else
+                ob_clean();
+
+                $sql = "SELECT * from Course ORDER BY year DESC";
+                $result = $dbconn->query($sql);
+                echo "<table class='form_div'>";
+                echo "<tr><td>Course code</td><td>Year</td><td>Title</td><td>Action</td></tr>";
+                while ($row = $result->fetch_assoc())
+                {
+                    echo "<tr><td>{$row['coursecode']}</td><td>{$row['year']}</td><td>{$row['title']}</td>
+                        <td>
+                            <form action=\"courseinfo.php\" method=\"GET\">
+                                <input type='hidden' name='coursecode' value={$row['coursecode']}>
+                                <input type='hidden' name='coursetitle' value={$row['title']}>
+                                <input type='hidden' name='year' value={$row['year']}>
+                                <input type=\"submit\" value=\"Show courseinfo\">
+                            </form></td>
+                            <td><form action=\"courseedit.php\" method=\"GET\">
+                                <input type='hidden' name='editCourse' value={$row['coursecode']}>
+                                <input type='submit' value=\"Edit\">                
+                            </form>
+                        </td><td><form method=\"GET\">
+                                <input type='hidden' name='deletecourse' value='1'>
+                                <input type='hidden' name='coursecode' value={$row['coursecode']}>
+                                <input type='hidden' name='year' value={$row['year']}>
+                                <input type='submit' value=\"Delete\">                
+                            </form>
+                        </td></tr>";
+                }
+                echo "</table>";
+            }
+
+            function AddCourse()
             {
-                echo "<p>There was a problem adding the new course.</p>";
+                ob_clean();
+
+                echo "
+                        <form method='GET'>
+                            <p>Course code (four letteres):</p>
+                            <input name='coursecode' type='text' maxlength='4' required>
+                            
+                            <p>What year is this course run:</p>
+                            <input name='courseyear' type='number' min='1000' max='".date("Y")."' required>
+                            
+                            <p>Title of course:</p>
+                            <input name='coursetitle' type='text' required>
+                            
+                            <input type='submit' value='Add course'>
+                        </form>";
             }
-        } else
-        {
-            echo "<p>Invalid input. Course code must be exactly four characters, year must be valid, and none of the input-fields can be null.</p>";
-        }
-    }
 
-    function Search()
-    {
-        ob_clean();
-        echo "
-            <form method='GET'>
-                <p>Search by course code:</p><br>
-                <input name='searchcode' type='search'>
-                <input type='submit' value='Search'>
-            </form>
-            ";
-    }
+            function AddNewCourse($coursecode, $courseyear, $coursetitle, $dbconn)
+            {
 
-    function SearchResult($id, $dbconn)
-    {
-        ob_clean();
-        $sql = "SELECT * from Course WHERE coursecode='$id' ORDER BY year DESC";
-        $result = $dbconn->query($sql);
-
-        echo "<table class='form_div'>";
-        echo "<tr><td>Course code</td><td>Year</td><td>Title</td><td>Action</td></tr>";
-        while ($row = $result->fetch_assoc())
-        {
-            $courseTitle=urlencode($row['title']);
-            echo "<tr><td>{$row['coursecode']}</td><td>{$row['year']}</td><td>{$row['title']}</td>
-                <td>
-                    <form action=\"courseinfo.php\" method=\"GET\">
-                        <input type='hidden' name='coursecode' value='$courseTitle'>
-                        <input type='hidden' name='coursetitle' value={$row['title']}>
-                        <input type='hidden' name='year' value={$row['year']}>
-                        <input type=\"submit\" value=\"Show courseinfo\">
-                    </form></td>
-                    <td><form action=\"courseedit.php\" method=\"GET\">
-                        <input type='hidden' name='editCourse' value={$row['coursecode']}>
-                        <input type='submit' value=\"Edit\">                
-                    </form>
-                </td><td><form method=\"GET\">
-                        <input type='hidden' name='deletecourse' value='1'>
-                        <input type='hidden' name='coursecode' value={$row['coursecode']}>
-                        <input type='hidden' name='year' value={$row['year']}>
-                        <input type='submit' value=\"Delete\">                
-                    </form>
-                </td></tr>";
-        }
-        echo "</table>";
-    }
-
-    if (isset($_GET["selectInfo"]))
-    {
-        $info = $_GET["selectInfo"];
-        if ($info=="1") Search();
-        elseif ($info=="2") EveryCourse($dbconn);
-        else AddCourse();
-    }
-
-    if (isset($_GET['deletecourse'])){
-        ob_clean();
-
-        $dbconn->query("START TRANSACTION");
-
-        $sql1 = "Delete from Grade where coursecode='{$_GET['coursecode']}' and year={$_GET['year']};";
-        if ($dbconn->query($sql1)===TRUE){
-            $sql2="Delete from Course where coursecode='{$_GET['coursecode']}' and year={$_GET['year']};";
-            if ($dbconn->query($sql2)===TRUE){
-                $dbconn->query("COMMIT");
-                echo "<p>Succesfully deleted the course!</p>";
+                /* Regular expressions to check if input is valid. */
+                $codepattern = "[a-zA-Z]{4}";
+                $titlepattern = "[a-zA-Z]+";
+                $yearpattern = "[1-2][0-9]{3}";
+                $coursecode = strtoupper($coursecode); //Makes sure course-code is uppercase.
+                ob_clean();
+                if (sizeof(preg_match($codepattern, $coursecode)) == 1 && sizeof(preg_match($titlepattern, $coursetitle)) == 1 && sizeof(preg_match($yearpattern, $courseyear)) == 1)
+                {
+                    $sql = "INSERT INTO Course VALUES ('$coursecode', '$courseyear', '$coursetitle')";
+                    if ($result = $dbconn->query($sql) === TRUE)
+                    {
+                        echo "<p>Course was successcully added.</p>";
+                    } else
+                    {
+                        echo "<p>There was a problem adding the new course.</p>";
+                    }
+                } else
+                {
+                    echo "<p>Invalid input. Course code must be exactly four characters, year must be valid, and none of the input-fields can be null.</p>";
+                }
             }
-        }else {
-            $dbconn->query("ROLLBACK");
-            echo "<p>Something went wrong. The course was not deleted.</p>";
-        }
-    } elseif (isset($_GET["coursecode"]) && isset($_GET["courseyear"]) && isset($_GET["coursetitle"]))
-    {
-        AddNewCourse($_GET["coursecode"], $_GET["courseyear"], $_GET["coursetitle"], $dbconn);
-    } elseif (isset($_GET["searchcode"]))
-    {
-        SearchResult($_GET["searchcode"], $dbconn);
-    }
+
+            function Search()
+            {
+                ob_clean();
+                echo "
+                    <form method='GET'>
+                        <p>Search by course code:</p><br>
+                        <input name='searchcode' type='search'>
+                        <input type='submit' value='Search'>
+                    </form>
+                    ";
+            }
+
+            function SearchResult($id, $dbconn)
+            {
+                ob_clean();
+                $sql = "SELECT * from Course WHERE coursecode='$id' ORDER BY year DESC";
+                $result = $dbconn->query($sql);
+
+                echo "<table class='form_div'>";
+                echo "<tr><td>Course code</td><td>Year</td><td>Title</td><td>Action</td></tr>";
+                while ($row = $result->fetch_assoc())
+                {
+                    $courseTitle=urlencode($row['title']);
+                    echo "<tr><td>{$row['coursecode']}</td><td>{$row['year']}</td><td>{$row['title']}</td>
+                        <td>
+                            <form action=\"courseinfo.php\" method=\"GET\">
+                                <input type='hidden' name='coursecode' value='$courseTitle'>
+                                <input type='hidden' name='coursetitle' value={$row['title']}>
+                                <input type='hidden' name='year' value={$row['year']}>
+                                <input type=\"submit\" value=\"Show courseinfo\">
+                            </form></td>
+                            <td><form action=\"courseedit.php\" method=\"GET\">
+                                <input type='hidden' name='editCourse' value={$row['coursecode']}>
+                                <input type='submit' value=\"Edit\">                
+                            </form>
+                        </td><td><form method=\"GET\">
+                                <input type='hidden' name='deletecourse' value='1'>
+                                <input type='hidden' name='coursecode' value={$row['coursecode']}>
+                                <input type='hidden' name='year' value={$row['year']}>
+                                <input type='submit' value=\"Delete\">                
+                            </form>
+                        </td></tr>";
+                }
+                echo "</table>";
+            }
+
+            if (isset($_GET["selectInfo"]))
+            {
+                $info = $_GET["selectInfo"];
+                if ($info=="1") Search();
+                elseif ($info=="2") EveryCourse($dbconn);
+                else AddCourse();
+            }
+
+            if (isset($_GET['deletecourse'])){
+                ob_clean();
+
+                $dbconn->query("START TRANSACTION");
+
+                $sql1 = "Delete from Grade where coursecode='{$_GET['coursecode']}' and year={$_GET['year']};";
+                if ($dbconn->query($sql1)===TRUE){
+                    $sql2="Delete from Course where coursecode='{$_GET['coursecode']}' and year={$_GET['year']};";
+                    if ($dbconn->query($sql2)===TRUE){
+                        $dbconn->query("COMMIT");
+                        echo "<p>Succesfully deleted the course!</p>";
+                    }
+                }else {
+                    $dbconn->query("ROLLBACK");
+                    echo "<p>Something went wrong. The course was not deleted.</p>";
+                }
+            } elseif (isset($_GET["coursecode"]) && isset($_GET["courseyear"]) && isset($_GET["coursetitle"]))
+            {
+                AddNewCourse($_GET["coursecode"], $_GET["courseyear"], $_GET["coursetitle"], $dbconn);
+            } elseif (isset($_GET["searchcode"]))
+            {
+                SearchResult($_GET["searchcode"], $dbconn);
+            }
 
 
 
-    ?>
-</div>
+        ?>
+    </div>
 </body>
 
 <footer class="bottomofpage">
--- studentaddcourse.php
+++ studentaddcourse.php
@@ -6,107 +6,110 @@
 </head>
 
 <body>
-<div class="title">
-    <h1>HiOA student information system</h1>
-</div>
+    <div class="title">
+        <h1>HiOA student information system</h1>
+    </div>
 
-<div>
-    <a class="navigation" href="index.php">Home</a>
-    <a class="currentpage" href="student.php">Student</a>
-    <a class="navigation" href="course.php">Course</a>
-    <a class="navigation" href="studyprogram.php">Study program</a>
-</div>
-<div class="siteinfo">
-<?php
-ob_start();
-ini_set('display_errors',1);
-$stID=$_GET['stID'];
-//Connection to dats04-dbproxy
-$host="10.1.1.130";
-$user="webuser";
-$pw="welcomeunclebuild";
-$db="studentinfosys";
-$dbconn = new mysqli($host, $user, $pw, $db);
+    <div>
+        <a class="navigation" href="index.php">Home</a>
+        <a class="currentpage" href="student.php">Student</a>
+        <a class="navigation" href="course.php">Course</a>
+        <a class="navigation" href="studyprogram.php">Study program</a>
+    </div>
+    <div class="siteinfo">
+        <?php
+            ob_start();
+            ini_set('display_errors',1);
+            $stID=$_GET['stID'];
+            //Connection to dats04-dbproxy
+            $host="10.1.1.130";
+            $user="webuser";
+            $pw="welcomeunclebuild";
+            $db="studentinfosys";
+            $dbconn = new mysqli($host, $user, $pw, $db);
 
-$getTitle="select coursecode, title from Course group BY title;";
-$getTitlecourse = $dbconn->query($getTitle);
+            $getTitle="select coursecode, title from Course group BY title;";
+            $getTitlecourse = $dbconn->query($getTitle);
 
-echo "<form id=\"selectCourse\" method=\"GET\">
-        <select name=\"coursecode\">";
-            while($row=$getTitlecourse->fetch_assoc()){
-                echo "<option value=\"{$row['coursecode']}\" name='coursecode'>{$row['title']}</option>";
-            }
-        echo "</select>
-        <input type='hidden' name='stID' value='$stID'>
-        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
-    </form>";
-
-function selectYear($dbconn,$stID,$coursecode){
-//    ob_clean();
-    $getYear="Select year from Course where coursecode='$coursecode';";
-    $getYearcourse = $dbconn->query($getYear);
-    $row =[];
-    echo "<form id=\"selectYear\" method=\"GET\">
-        <select name=\"year\">";
-            while($row=$getYearcourse->fetch_assoc()){
-                echo "<option value=\"{$row['year']}\" name='year'>{$row['year']}</option>";
-            }
-        echo "</select>
-        <input type='hidden' name='coursecode' value='$coursecode'>
-        <input type='hidden' name='stID' value='$stID'>
-        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
-    </form>";
-}
-
-function chooseGrade($stID,$coursecode,$year){
-
-    echo "<form id=\"selectGrade\" method=\"GET\">
-        <select name=\"grade\">
-            <option value='A'>A</option>
-            <option value='B'>B</option>
-            <option value='C'>C</option>
-            <option value='D'>D</option>
-            <option value='E'>E</option>
-            <option value='F'>F</option>
-            <option value=''>Not finished</option>
-        </select>
-        <input type='hidden' name='coursecode' value='$coursecode'>
-        <input type='hidden' name='stID' value='$stID'>
-        <input type='hidden' name='year' value='$year'>
-        <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
-    </form>";
-}
-
-function insertGrade($dbconn, $stID, $coursecode, $year, $grade){
-    $insert="insert into Grade values ($stID,'$coursecode',$year,'$grade');";
-    if ($dbconn->query($insert)===TRUE){
-        echo "<p>Succesfully added $coursecode to student $stID with the grade: $grade</p>
-                <form action='studentinfo.php' method='get'>
-                    <input type='hidden' name='stID' value=$stID>
-                    <input type='submit' value='Back to student'>
+            echo "<p>Course:</p>
+                    <form id=\"selectCourse\" method=\"GET\">
+                    <select name=\"coursecode\">";
+                        while($row=$getTitlecourse->fetch_assoc()){
+                            echo "<option value=\"{$row['coursecode']}\" name='coursecode'>{$row['title']}</option>";
+                        }
+                    echo "</select>
+                    <input type='hidden' name='stID' value='$stID'>
+                    <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
                 </form>";
-    }else {
-        echo "<p>Something went wrong. The course was not added to student $stID</p>
-                <form action='studentinfo.php' method='get'>
-                    <input type='hidden' name='stID' value=$stID>
-                    <input type='submit' value='Back to student'>
+
+            function selectYear($dbconn,$stID,$coursecode){
+                $getYear="Select year from Course where coursecode='$coursecode';";
+                $getYearcourse = $dbconn->query($getYear);
+                $row =[];
+
+                echo "<p>What year did the student attend the course?</p>
+                    <form id=\"selectYear\" method=\"GET\">
+                    <select name=\"year\">";
+                        while($row=$getYearcourse->fetch_assoc()){
+                            echo "<option value=\"{$row['year']}\" name='year'>{$row['year']}</option>";
+                        }
+                    echo "</select>
+                    <input type='hidden' name='coursecode' value='$coursecode'>
+                    <input type='hidden' name='stID' value='$stID'>
+                    <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
                 </form>";
-    }
-}
+            }
+
+            function chooseGrade($stID,$coursecode,$year){
+
+                echo "<p>Select the grade received:</p>
+                    <form id=\"selectGrade\" method=\"GET\">
+                    <select name=\"grade\">
+                        <option value='A'>A</option>
+                        <option value='B'>B</option>
+                        <option value='C'>C</option>
+                        <option value='D'>D</option>
+                        <option value='E'>E</option>
+                        <option value='F'>F</option>
+                        <option value=''>Not finished</option>
+                    </select>
+                    <input type='hidden' name='coursecode' value='$coursecode'>
+                    <input type='hidden' name='stID' value='$stID'>
+                    <input type='hidden' name='year' value='$year'>
+                    <input class=\"dblock\" type=\"Submit\" value=\"Submit\">
+                </form>";
+            }
+
+            function insertGrade($dbconn, $stID, $coursecode, $year, $grade){
+                $insert="insert into Grade values ($stID,'$coursecode',$year,'$grade');";
+                if ($dbconn->query($insert)===TRUE){
+                    echo "<p>Succesfully added $coursecode to student $stID with the grade: $grade</p>
+                            <form action='studentinfo.php' method='get'>
+                                <input type='hidden' name='stID' value=$stID>
+                                <input type='submit' value='Back to student'>
+                            </form>";
+                }else {
+                    echo "<p>Something went wrong. The course was not added to student $stID</p>
+                            <form action='studentinfo.php' method='get'>
+                                <input type='hidden' name='stID' value=$stID>
+                                <input type='submit' value='Back to student'>
+                            </form>";
+                }
+            }
 
 
-if (isset($_GET["coursecode"])){
-    selectYear($dbconn,$_GET['stID'],$_GET['coursecode']);
-}
-if (isset($_GET["year"])){
-    chooseGrade($_GET['stID'],$_GET['coursecode'],$_GET['year']);
-}
-if (isset($_GET["grade"])){
-    insertGrade($dbconn, $_GET['stID'],$_GET['coursecode'],$_GET['year'],$_GET['grade']);
-}
+            if (isset($_GET["coursecode"])){
+                selectYear($dbconn,$_GET['stID'],$_GET['coursecode']);
+            }
+            if (isset($_GET["year"])){
+                chooseGrade($_GET['stID'],$_GET['coursecode'],$_GET['year']);
+            }
+            if (isset($_GET["grade"])){
+                insertGrade($dbconn, $_GET['stID'],$_GET['coursecode'],$_GET['year'],$_GET['grade']);
+            }
 
-?>
-</div>
+        ?>
+    </div>
 </body>
 
 <footer class="bottomofpage">
