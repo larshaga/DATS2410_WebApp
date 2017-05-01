00c60a166f1f74910a545b921bf72455ca3f7668
gustavberggren96 <gustavberggren96@yahoo.com>
2017-05-01 11:54:33 +0200

    Added regex test

--- studentedit.php
+++ studentedit.php
@@ -41,24 +41,26 @@ if(isset($_GET['stID']))
     {
         while ($row = $result->fetch_assoc())
         {
-            showStudent($row['fname'], $row['lname'], $row['email'],$id);
+            showStudent($row['fname'], $row['lname'], $row['email'],$id,"","","");
         }
     }
 }
 
 
-
-function showStudent($fname,$lname,$email,$id)
+function showStudent($fname,$lname,$email,$id,$fnErr,$lnErr,$emErr)
 {
     echo "<div class='form_div'>
             <form method='GET'>
                 <input type='hidden' name='stID' value='$id'>
                 <label for='fname'> Firstname: </label>
                 <input class='dblock' type='Text' name='fname' value='$fname'>
+                <p><span class='errorField'>$fnErr</span></p>
                 <label for='lname'> Lastname: </label>
                 <input class='dblock' type='Text' name='lname' value='$lname'>
+                <p><span class='errorField'>$lnErr</span></p>
                 <label for='email'> Email: </label>
                 <input class='dblock' type='Text' name='email' value='$email'>
+                <p><span class='errorField'>$emErr</span></p>
                 <input class='dblock' type='submit' value='Save'>
             </form>
           </div>";
@@ -66,30 +68,36 @@ function showStudent($fname,$lname,$email,$id)
 
 function updateStudent($fname,$lname,$email,$id,$dbconn)
 {
-    /*$namepattern = "[a-zA-Z]+";
-    $emailpattern = "[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+";
-        if(sizeof(preg_match($namepattern, $lname)) == 1 && sizeof(preg_match($namepattern, $fname)) == 1 && sizeof(preg_match($emailpattern, $email)) == 1)
-            {*/
-
-    $dbconn;
-    $sql2 = "UPDATE Student SET firstname='$fname',lastname='$lname',email='$email' WHERE stID='$id'";
-    if ($dbconn->query($sql2) === TRUE)
+    $namepattern = "/^[a-zA-Z]+$/";
+    $emailpattern = "/^[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+$/";
+    if(preg_match($namepattern, $lname) && preg_match($namepattern, $fname) && preg_match($emailpattern, $email))
     {
         ob_clean();
-        showStudent($fname, $lname, $email,$id);
-        echo "<p>Edit successfull!</p>
-            <form action='studentinfo.php' method='get'>
-                <input type='hidden' name='stID' value=$id>
-                <input type='submit' value='Back to student'>
-            </form>";
+        $fnErr = $lnErr = $emErr = "";
+        if (preg_match($namepattern, $fname))$fnErr="Invalid firstname input";
+        if (preg_match($namepattern, $lname))$lnErr="Invalid lastname input";
+        if (preg_match($namepattern, $email))$emErr="Invalid email input";
+        showStudent($fname,$lname,$email,$id,$fnErr,$lnErr,$emErr);
     }
-    else
-    {
-        echo "<p>Failed to save changes</p>
+    else {
+
+        $dbconn;
+        $sql2 = "UPDATE Student SET firstname='$fname',lastname='$lname',email='$email' WHERE stID='$id'";
+        if ($dbconn->query($sql2) === TRUE) {
+            ob_clean();
+            showStudent($fname, $lname, $email, $id,"","","");
+            echo "<p>Edit successfull!</p>
             <form action='studentinfo.php' method='get'>
                 <input type='hidden' name='stID' value=$id>
                 <input type='submit' value='Back to student'>
             </form>";
+        } else {
+            echo "<p>Failed to save changes</p>
+            <form action='studentinfo.php' method='get'>
+                <input type='hidden' name='stID' value=$id>
+                <input type='submit' value='Back to student'>
+            </form>";
+        }
     }
 }
 if(isset($_GET['fname']) && isset($_GET['lname']) && isset($_GET['email']) && isset($_GET['stID']))
